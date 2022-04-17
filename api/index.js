45c89e18800bd229dc8a81f71ddac23b09847947
aminos02 <azzouzamine02@gmail.com>
2022-04-17 15:07:43 +0200

    second

--- api/index.js
+++ api/index.js
@@ -1,14 +1,130 @@
-const app=require("express")();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.get("/",(req,res)=>res.send("hello"));
+const app = express();
 
-app.get("/stream",(req,res)=>{
-    res.setHeader("Content-Type","text/event-stream");
-    res.write("data: lola\n\n")
-}
-);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/status', (request, response) => response.json({clients: clients.length}));
+
+const PORT = 3001;
+
+let clients = [];
+let facts = [];
+
+function eventsHandler(request, response, next) {
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+  
+    const data = `data: ${JSON.stringify(facts)}\n\n`;
+  
+    response.write(data);
+  
+    const clientId = Date.now();
+  
+    const newClient = {
+      id: clientId,
+      response
+    };
+  
+    clients.push(newClient);
+  
+    request.on('close', () => {
+      console.log(`${clientId} Connection closed`);
+      clients = clients.filter(client => client.id !== clientId);
+    });
+  }
+  function sendEventsToAll(newFact) {
+    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+  }
+  var i=0;
+  async function addFact(request, respsonse, next) {
+    const newFact = request.body;
+    console.log(request.body)
+    facts.push(newFact);
+    respsonse.json(newFact)
+    return sendEventsToAll(i++);
+  }
+  
+  
+  app.post('/fact', addFact);
+  app.get('/events', eventsHandler);
 
 
-app.listen(8080);
 
-console.log("Listening on 8080")
+app.listen(PORT, () => {
+  console.log(`Facts Events service listening at http://localhost:${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app=require("express")();
+// const cors=require("cors")
+
+// app.use(cors())
+
+
+
+// app.get("/stream",(req,res)=>{
+//     res.setHeader("Content-Type","text/event-stream");
+//     send(res)
+// }
+// );
+// var i=0;
+// function send(res){
+//     res.write("hello world ");
+//     i++;
+//     if(i<=5)
+//     setTimeout(()=>{
+//         send(res)
+//     },1000)
+// }
+
+
+// app.listen(8080);
+
+// console.log("Listening on 8080")
--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [ facts, setFacts ] = useState([]);
+  const [ listening, setListening ] = useState(false);
+
+  useEffect( () => {
+    if (!listening) {
+      const events = new EventSource('http://localhost:3001/events');
+
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+        console.log(parsedData)
+        setFacts((facts) => facts.push(parsedData));
+      };
+
+      setListening(true);
+    }
+  }, [listening, facts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Welcome</h1>
+      <ul>
+        {facts.map((item,index)=><li  key={index}>
+          {item}
+        </li>)}
+      </ul>
+
     </div>
   );
 }
